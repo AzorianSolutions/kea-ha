1c48c06a0a5ea077a12b0904e712c7d918bdc215
Matt Scott <matt@azorian.solutions>
2024-01-07 11:10:47 -0500

    Rounded off recent conventions changes to reduce definition redundancy and simplify implementation.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -10,9 +10,6 @@ ROOT_PATH: Path = Path(os.getcwd())
 DEFAULT_CONFIG_PATH: Path = ROOT_PATH / 'defaults.yml'
 """ The file containing the default YAML configuration to use when an environment specific file is missing. """
 
-SRC_PATH: Path = ROOT_PATH / 'src'
-""" The source path of the application which is typically the src directory within the ROOT_PATH. """
-
 DEFAULT_ENV_PATH: Path = ROOT_PATH / '.env'
 """ The default path to the environment file to load settings from. """
 
@@ -22,24 +19,19 @@ DEFAULT_ENV_FILE_ENCODING: str = 'UTF-8'
 DEFAULT_SECRETS_PATH: Path | None = None
 """ The default path to the secrets directory to load environment variable values from. """
 
-base_config: dict[str, typing.Any] = ConfigLoader.load_yaml(DEFAULT_CONFIG_PATH)
 
-
-def load_settings(env_file_path: str | Path | None = None, env_file_encoding: str | None = None,
-                  secrets_path: str | Path | None = None) -> AppSettings:
+def load_settings(env_prefix: str) -> AppSettings:
     """ Loads an AppSettings instance based on the given environment file and secrets directory. """
+    from loguru import logger
 
     # Extract the default environment file path from the environment if defined, otherwise use the default path
-    if env_file_path is None:
-        env_file_path = os.getenv(f'{base_config["project"]["environment"]["prefix"]}_ENV_FILE', DEFAULT_ENV_PATH)
+    env_file_path = os.getenv(f'{env_prefix}_ENV_FILE', DEFAULT_ENV_PATH)
 
     # Extract the default environment file encoding from the environment if defined, otherwise use the default value
-    if env_file_encoding is None:
-        env_file_encoding = os.getenv(f'{base_config["project"]["environment"]["prefix"]}_ENV_FILE_ENCODING', DEFAULT_ENV_FILE_ENCODING)
+    env_file_encoding = os.getenv(f'{env_prefix}_ENV_FILE_ENCODING', DEFAULT_ENV_FILE_ENCODING)
 
     # Extract the default secrets directory path from the environment if defined, otherwise use the default path
-    if secrets_path is None:
-        secrets_path = os.getenv(f'{base_config["project"]["environment"]["prefix"]}_ENV_SECRETS_DIR', DEFAULT_SECRETS_PATH)
+    secrets_path = os.getenv(f'{env_prefix}_ENV_SECRETS_DIR', DEFAULT_SECRETS_PATH)
 
     if env_file_path is not None and not isinstance(env_file_path, Path):
         env_file_path = Path(env_file_path)
@@ -48,31 +40,33 @@ def load_settings(env_file_path: str | Path | None = None, env_file_encoding: st
         secrets_path = Path(secrets_path)
 
     params: dict = {
-        '_env_file': env_file_path,
-        '_env_file_encoding': env_file_encoding,
+        'root_path': ROOT_PATH,
+        'env_file': env_file_path,
+        'env_file_encoding': env_file_encoding,
     }
 
-    os.putenv(f'{base_config["project"]["environment"]["prefix"]}_ENV_FILE', str(env_file_path))
-    os.putenv(f'{base_config["project"]["environment"]["prefix"]}_ENV_FILE_ENCODING', env_file_encoding)
+    # Ensure any default values get pushed back into the environment
+    os.putenv(f'{env_prefix}_ENV_FILE', str(env_file_path))
+    os.putenv(f'{env_prefix}_ENV_FILE_ENCODING', env_file_encoding)
 
     if secrets_path is not None:
         valid: bool = True
 
         if not secrets_path.exists():
             valid = False
-            print(f'The given path for the "--secrets-dir" option does not exist: {secrets_path}')
+            logger.warning(f'The given path for the "--secrets-dir" option does not exist: {secrets_path}')
         elif not secrets_path.is_dir():
             valid = False
-            print(f'The given path for the "--secrets-dir" option is not a directory: {secrets_path}')
+            logger.warning(f'The given path for the "--secrets-dir" option is not a directory: {secrets_path}')
 
         if valid:
-            params['_secrets_dir'] = secrets_path
-            os.putenv(f'{base_config["project"]["environment"]["prefix"]}_ENV_SECRETS_DIR', str(secrets_path))
+            params['secrets_dir'] = secrets_path
+            # Ensure the default value gets pushed back into the environment
+            os.putenv(f'{env_prefix}_ENV_SECRETS_DIR', str(secrets_path))
 
-    # Load base app configuration settings from the given environment file and the local environment
-    app_settings = AppSettings(**params)
-
-    return app_settings
+    return AppSettings(**params)
 
 
-settings: AppSettings = load_settings()
+base_config: dict[str, typing.Any] = ConfigLoader.load_yaml(DEFAULT_CONFIG_PATH)
+
+settings: AppSettings = load_settings(env_prefix=base_config['project']['environment']['prefix'])
--- src/app/model/settings.py
+++ src/app/model/settings.py
@@ -1,30 +1,10 @@
 import os
 from pathlib import Path
-from pydantic.fields import FieldInfo
-from pydantic_settings import (
-    EnvSettingsSource, BaseSettings, PydanticBaseSettingsSource
-)
-from typing import Any, Type, Tuple
+from pydantic_settings import BaseSettings
 from app.util.config import ConfigLoader
 
 DEFAULT_CONFIG_PATH: Path = Path('defaults.yml')
 base_config: dict = ConfigLoader.load_yaml(DEFAULT_CONFIG_PATH)
-
-
-class SettingsSource(EnvSettingsSource):
-    def prepare_field_value(
-            self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool
-    ) -> Any:
-        path_fields = ['config_path', 'env_file', 'env_secrets_dir', 'root_path']
-
-        if field_name == 'root_path' and value in ['.', './']:
-            print('TEST TEST TEST')
-            value = Path(os.getcwd())
-
-        if field_name in path_fields and value is not None and not isinstance(value, Path):
-            value = Path(value)
-
-        return value
 
 
 class AppSettings(BaseSettings):
@@ -33,19 +13,20 @@ class AppSettings(BaseSettings):
     _config: dict | None = None
     """ Additional configuration settings loaded automatically from the YAML file given in the config_path setting. """
 
-    _root_path: Path = Path(os.getcwd())
-    """ The root path of the application which is typically the project repository root directory. """
-
     config_path: str | Path = 'config.yml'
     debug: bool = False
-    env_file: str | Path = '.env'
+    env_file: str | Path | None = None
     env_file_encoding: str = 'UTF-8'
     env_secrets_dir: str | Path | None = None
+
+    root_path: str | Path = Path(os.getcwd())
+    """ The root path of the application which is typically the project repository root directory. """
+
     version: str = base_config['project']['version']
     """ The application version number """
 
     @property
-    def config(self) -> dict[str, Any]:
+    def config(self) -> dict:
         """ Returns the configuration data provided by the YAML file given in the config_path setting. """
 
         # Load the YAML configuration file if it exists and has not already been loaded
@@ -61,22 +42,6 @@ class AppSettings(BaseSettings):
 
         return self._config
 
-    @property
-    def root_path(self) -> Path:
-        """ Returns the root path of the application which is typically the project repository root directory. """
-        return self._root_path
-
     class Config:
         env_prefix = base_config['project']['environment']['prefix'] + '_'
         env_nested_delimiter = '__'
-
-    @classmethod
-    def settings_customise_sources(
-            cls,
-            settings_cls: Type[BaseSettings],
-            init_settings: PydanticBaseSettingsSource,
-            env_settings: PydanticBaseSettingsSource,
-            dotenv_settings: PydanticBaseSettingsSource,
-            file_secret_settings: PydanticBaseSettingsSource,
-    ) -> Tuple[PydanticBaseSettingsSource, ...]:
-        return (SettingsSource(settings_cls),)
