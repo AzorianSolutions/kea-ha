8c2e10851eff408741d8dac3847ef2b4ff3a582e
Matt Scott <matt@azorian.solutions>
2024-01-08 15:14:39 -0500

    Updated Dockerfile and entrypoint.sh to finish removing the dynamic conf file generation from the solution.

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 DB_CONNECT_RETRIES=20
-DB_BACKEND=mysql
+DB_BACKEND="${KEA__BACKEND__TYPE}"
 DB_USER=root
-DB_PASSWORD="${KEA_DB_ROOT_PASSWORD}"
+DB_PASSWORD="${KEA__BACKEND__OPTS__ROOT_PASSWORD}"
 
 configure_kea_admin () {
-  if [ "$KEA_DB_TYPE" == "postgresql" ]; then
-    DB_BACKEND=pgsql
-    DB_USER="${KEA_DB_USER}"
-    DB_PASSWORD="${KEA_DB_PASSWORD}"
+  if [ "${KEA__BACKEND__TYPE}" == "postgresql" ]; then
+    DB_USER="${KEA__BACKEND__OPTS__USERNAME}"
+    DB_PASSWORD="${KEA__BACKEND__OPTS__PASSWORD}"
   fi
 }
 
@@ -34,7 +33,7 @@ initialize_db () {
   configure_kea_admin
 
   # Run the database initialization process provided by the kea-admin tool
-  kea-admin db-init "${DB_BACKEND}" -h "${KEA_DB_HOST}" -P "${KEA_DB_PORT}" -u "${DB_USER}" -p "${DB_PASSWORD}" -n "${KEA_DB_NAME}"
+  kea-admin db-init "${DB_BACKEND}" -h "${KEA__BACKEND__OPTS__HOST}" -P "${KEA__BACKEND__OPTS__PORT}" -u "${DB_USER}" -p "${DB_PASSWORD}" -n "${KEA__BACKEND__OPTS__NAME}"
 }
 
 upgrade_db () {
@@ -42,7 +41,7 @@ upgrade_db () {
   configure_kea_admin
 
   # run the databaser upgrade process provided by the kea-admin tool
-  kea-admin db-upgrade "${DB_BACKEND}" -h "${KEA_DB_HOST}" -P "${KEA_DB_PORT}" -u "${DB_USER}" -p "${DB_PASSWORD}" -n "${KEA_DB_NAME}"
+  kea-admin db-upgrade "${DB_BACKEND}" -h "${KEA__BACKEND__OPTS__HOST}" -P "${KEA__BACKEND__OPTS__PORT}" -u "${DB_USER}" -p "${DB_PASSWORD}" -n "${KEA__BACKEND__OPTS__NAME}"
 }
 
 echo "Initializing application environment..."
@@ -51,12 +50,12 @@ echo "Initializing application environment..."
 initialize_app
 
 # If using the MySQL engine, then check if the kea database exists. If not, create it.
-if [ "$KEA_DB_TYPE" == "mysql" ]; then
+if [ "$KEA__BACKEND__TYPE" == "mysql" ]; then
 
   echo "Initializing MySQL database..."
 
   # Wait for the MySQL server to become available
-  until mysqladmin ping -h "${KEA_DB_HOST}" -P "${KEA_DB_PORT}" -u root -p"${KEA_DB_ROOT_PASSWORD}" --silent || [ $DB_CONNECT_RETRIES -eq 0 ]; do
+  until mysqladmin ping -h "${KEA__BACKEND__OPTS__HOST}" -P "${KEA__BACKEND__OPTS__PORT}" -u root -p"${KEA__BACKEND__OPTS__ROOT_PASSWORD}" --silent || [ $DB_CONNECT_RETRIES -eq 0 ]; do
     echo "Waiting for MySQL database server to become available, $((DB_CONNECT_RETRIES)) remaining attempts..."
     DB_CONNECT_RETRIES=$((DB_CONNECT_RETRIES-=1))
     sleep 1
@@ -65,8 +64,8 @@ if [ "$KEA_DB_TYPE" == "mysql" ]; then
   echo "Checking if database already initialized..."
 
   # Check if the "lease4" tables exists in the Kea database as a rudimentary check to see if the database is ready
-  table_sql="SHOW TABLES FROM ${KEA_DB_NAME}"
-  tables_exist=$(mysql -h "${KEA_DB_HOST}" -P "${KEA_DB_PORT}" -u root -p"${KEA_DB_ROOT_PASSWORD}" -e "$table_sql" | grep "lease4" > /dev/null; echo "$?")
+  table_sql="SHOW TABLES FROM ${KEA__BACKEND__OPTS__NAME}"
+  tables_exist=$(mysql -h "${KEA__BACKEND__OPTS__HOST}" -P "${KEA__BACKEND__OPTS__PORT}" -u root -p"${KEA__BACKEND__OPTS__ROOT_PASSWORD}" -e "$table_sql" | grep "lease4" > /dev/null; echo "$?")
 
   # If the kea database is not seemingly ready, run the database initialization and upgrade processes
   if [ "$tables_exist" != "0" ]; then
@@ -80,16 +79,16 @@ if [ "$KEA_DB_TYPE" == "mysql" ]; then
 fi
 
 # If using the Postgres engine, create the kea user and database with appropriate privileges
-if [ "$KEA_DB_TYPE" == "postgresql" ]; then
+if [ "$KEA__BACKEND__TYPE" == "pgsql" ]; then
 
   echo "Initializing Postgres database..."
 
   export PGPASSWORD="${KEA_DB_PASSWORD}"
 
-  test_string="${KEA_DB_HOST}:${KEA_DB_PORT} - accepting connections"
+  test_string="${KEA__BACKEND__OPTS__HOST}:${KEA__BACKEND__OPTS__PORT} - accepting connections"
 
   # Wait for the Postgres server to become available
-  until [ "$(pg_isready -h "${KEA_DB_HOST}" -p "${KEA_DB_PORT}" -U "${KEA_DB_USER}" -d "${KEA_DB_NAME}")" == "$test_string" ] || [ $DB_CONNECT_RETRIES -eq 0 ]; do
+  until [ "$(pg_isready -h "${KEA__BACKEND__OPTS__HOST}" -p "${KEA__BACKEND__OPTS__PORT}" -U "${KEA_DB_USER}" -d "${KEA__BACKEND__OPTS__NAME}")" == "$test_string" ] || [ $DB_CONNECT_RETRIES -eq 0 ]; do
     echo "Waiting for Postgres database server to become available, $((DB_CONNECT_RETRIES)) remaining attempts..."
     DB_CONNECT_RETRIES=$((DB_CONNECT_RETRIES-=1))
     sleep 1
@@ -97,7 +96,7 @@ if [ "$KEA_DB_TYPE" == "postgresql" ]; then
 
   # Check if the "lease4" tables exists in the Kea database as a rudimentary check to see if the database is ready
   table_sql="SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = '${KEA_DB_SCHEMA}' AND table_name = 'lease4');"
-  table_exists=$(psql -h "${KEA_DB_HOST}" -p "${KEA_DB_PORT}" -U "${KEA_DB_USER}" -d "${KEA_DB_NAME}" -c "$table_sql" | grep "f" > /dev/null; echo "$?")
+  table_exists=$(psql -h "${KEA__BACKEND__OPTS__HOST}" -p "${KEA__BACKEND__OPTS__PORT}" -U "${KEA_DB_USER}" -d "${KEA__BACKEND__OPTS__NAME}" -c "$table_sql" | grep "f" > /dev/null; echo "$?")
 
   # If the kea database is not seemingly ready, run the database initialization and upgrade processes
   if [ "$table_exists" == "0" ]; then
