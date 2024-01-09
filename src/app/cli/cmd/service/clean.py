f56354db43991ab59d1ace83760f2a78ea8f5cf3
Matt Scott <matt@azorian.solutions>
2024-01-09 10:04:11 -0500

    Added clean commands for the image and service groups. Each cleans their respective build files from the build path (excluding service conf files).

---
+++ src/app/cli/cmd/service/clean.py
@@ -0,0 +1,42 @@
+import click
+import os
+from loguru import logger
+from pathlib import Path
+from subprocess import CompletedProcess
+from app.cli.entry import pass_environment, confirm_option
+from app.model.cli import Environment
+from app.util.config import ConfigBuilder
+from . import group
+
+
+@group.command('clean')
+@confirm_option
+@pass_environment
+def wrapper(env: Environment, yes: bool):
+    """Cleans the container service build path."""
+    return command(env, yes)
+
+
+def command(env: Environment, yes: bool) -> CompletedProcess | bool:
+    """Cleans the container service build path."""
+
+    if not yes:
+        confirm = click.confirm('Are you sure you want to clean the container service build path?', default=None)
+
+        if not confirm:
+            logger.warning('Aborting container service build path cleaning process for lack of user confirmation '
+                           + 'or the `-y` flag.')
+            return False
+
+    compose_file = Path(env.settings.c('service/paths/compose/file'))
+    env_file = Path(env.settings.c('service/paths/compose/env'))
+
+    if compose_file.exists():
+        os.remove(compose_file)
+        logger.debug(f'Removed compose file: {compose_file}')
+
+    if env_file.exists():
+        os.remove(env_file)
+        logger.debug(f'Removed env file: {env_file}')
+
+    logger.success('Successfully cleaned the container service build path.')
