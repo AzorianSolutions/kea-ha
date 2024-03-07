77be6c7849539505c61ee58e3111b49f1ba98528
Matt Scott <matt@azorian.solutions>
2024-03-07 09:42:20 -0500

    Updated setup tools script to use new configuration implementation.

--- setup.py
+++ setup.py
@@ -1,30 +1,41 @@
-import yaml
+import subprocess
+import sys
 from setuptools import setup
 
-config = {}
 
-with open('defaults.yml') as f:
-    config.update(yaml.load(f, Loader=yaml.FullLoader))
-    f.close()
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-with open('requirements.txt') as f:
-    required_packages = f.read().splitlines()
-    f.close()
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-    f.close()
+def load_config() -> dict:
+    import yaml
+    with open('defaults.yml') as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def load_requirements() -> list:
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+
+def load_readme() -> str:
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+install('pyyaml')
+config = load_config()
 
 setup(
     name=config['app']['name'],
     version=config['app']['version'],
     package_dir={'': 'src'},
-    install_requires=required_packages,
+    install_requires=load_requirements(),
     entry_points={
         'console_scripts': [
             config['app']['cli']['entrypoint'] + ' = app.cli.entry:cli',
         ],
     },
-    long_description=long_description,
+    long_description=load_readme(),
     long_description_content_type='text/markdown',
 )
